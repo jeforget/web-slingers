e80110f885efc3289d3b193e2c81b527d19edc74
Ho Van Thanh <toilathanh1504qn@gmail.com>
2024-03-19 02:27:35 -0400

    add register page and set up database to save username, email, password

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
-from flask import Flask, render_template, url_for, request
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from pymongo import MongoClient
 app = Flask(__name__)
+
+app.secret_key = '13513ijnijdsuia7safv'
+
+# Connect to MongoDB
+client = MongoClient('mongo')
+db = client['312_database']
+collection = db['users']
+
 
 @app.route('/', methods=['POST','GET'])
 def index():
     return render_template("index.html")
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if user already exists
+        if collection.find_one({'email': email}) or collection.find_one({"username": username}):
+            flash('Email or username already exists!')
+            return redirect(url_for('index'))
+
+        # If user does not exist, insert into database
+        user_data = {
+            'username': username,
+            'email': email,
+            'password': password
+        }
+        collection.insert_one(user_data)
+        flash('Registration successful!')
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+
 @app.route('/page1')
 def page1():
     return render_template("page1.html")
+
 
 @app.route('/page2')
 def page2():
     return "Page 2 doesn't exist yet"
 
-@app.route('/page3')
-def page3():
-    return "Page 3 doesn't exist yet"
-
-@app.route('/page4')
-def page4():
-    return "Page 4 doesn't exist yet"
 
 @app.after_request
 def set_response_headers(response):
